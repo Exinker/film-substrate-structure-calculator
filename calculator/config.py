4196ba99e47d4aaf9c70edcc15aa7880436147ed
Exinker <exinker@gmail.com>
2024-06-11 18:16:26 +0700

    feat: remove default method

--- calculator/config.py
+++ calculator/config.py
@@ -3,6 +3,7 @@ import json
 from dataclasses import dataclass
 from typing import Literal
 
+from calculator.exceptions import ConfigLoadError
 from calculator.types import GPa, Meter, MicroMeter
 
 
@@ -25,27 +26,19 @@ class Config:
     young_module: GPa
     stress_sign: Literal[-1, +1]
 
-    def save(self, name: str) -> None:
+    def save(self, name: str = 'config') -> None:
         dat = dataclasses.asdict(self)
 
         with open(f'{name}.json', 'w') as file:
             json.dump(dat, file)
 
     @classmethod
-    def default(cls) -> 'Config':
-        return cls(
-            radius_standart=20.3,
-            radius_flat=-197,
-            thickness_film=0.191,
-            thickness_substrate=390,
-            young_module=180.5,
-            stress_sign=-1,
-        )
-
-    @classmethod
     def load(cls, name: str = 'config') -> 'Config':
 
-        with open(f'{name}.json', 'r') as file:
-            dat = json.load(file)
+        try:
+            with open(f'{name}.json', 'r') as file:
+                dat = json.load(file)
+        except FileNotFoundError:
+            raise ConfigLoadError('Файл `config.json` не найден!')
 
         return cls(**dat)
