a5772ffa6d9ff97b8ef028875acfd96e2594932d
Pavel Vashchenko <exinker@gmail.com>
2024-06-17 08:01:14 +0700

    feat: major updates

--- calculator/report.py
+++ calculator/report.py
@@ -5,47 +5,112 @@ import numpy as np
 import pandas as pd
 
 from calculator.config import Config
-from calculator.length import Distance
+from calculator.curvature import Curvature
+from calculator.length import LengthMap
+from calculator.stress import Stress
 
 
 @dataclass(frozen=True, slots=True)
 class Report:
-    length: Distance
+    name: str
     config: Config
 
     def create(self) -> None:
-        filedir = self.length.filedir
+        filedir = os.path.join(os.getcwd(), 'data', self.name)
         filepath = os.path.join(filedir, 'report.xlsx')
 
-        # results sheet
-        _, name = os.path.split(filedir)
+        # calculations
+        length = LengthMap.calculate(name=self.name)
+        curvature = Curvature.calculate(
+            length=length,
+            config=self.config,
+        )
+        stress = Stress.calculate(
+            curvature=curvature.value,
+            config=self.config,
+        )
 
+        # results sheet
         frame = pd.concat([
             pd.DataFrame({
                 'index': ['Ср. знач.', 'Дов. инт.'],
-                'l, мкм': [self.length.stats.value, self.length.stats.interval],
+                'l, мкм': [length['sample'].stats.value, length['sample'].stats.interval],
+                'K, м-1': [curvature.stats.value, curvature.stats.interval],
+                'σ, МПа': [stress.stats.value, stress.stats.interval],
             }).set_index('index', drop=True),
             pd.DataFrame({
                 'index': [''],
                 'l, мкм': [''],
+                'K, м-1': [''],
+                'σ, МПа': [''],
             }).set_index('index', drop=True),
             pd.DataFrame({
-                'index': np.arange(1, len(self.length.value)+1),
-                'l, мкм': self.length.value,
+                'index': np.arange(1, len(length['sample'].value)+1),
+                'l, мкм': length['sample'].value,
+                'K, м-1': curvature.value,
+                'σ, МПа': stress.value,
             }).set_index('index', drop=True),
         ])
         frame.index.name = ''
-        frame.columns = pd.MultiIndex.from_product([[name], frame.columns])
+        frame.columns = pd.MultiIndex.from_product([[self.name], frame.columns])
 
         mode = 'a' if os.path.isfile(filepath) else 'w'
         if_sheet_exists = 'replace' if mode == 'a' else None
         with pd.ExcelWriter(filepath, mode=mode, if_sheet_exists=if_sheet_exists, engine='openpyxl') as writer:
             frame.to_excel(
                 writer,
-                sheet_name=name,
+                sheet_name=self.name,
             )
 
-        # calibration sheet
+        # ref-standard sheet
+        frame = pd.concat([
+            pd.DataFrame({
+                'index': ['Ср. знач.', 'Дов. инт.'],
+                'l_эт, мкм': [length['ref-standard'].stats.value, length['ref-standard'].stats.interval],
+            }).set_index('index', drop=True),
+            pd.DataFrame({
+                'index': [''],
+                'l_эт, мкм': [''],
+            }).set_index('index', drop=True),
+            pd.DataFrame({
+                'index': np.arange(1, len(length['ref-standard'].value)+1),
+                'l_эт, мкм': length['ref-standard'].value,
+            }).set_index('index', drop=True),
+        ])
+        frame.index.name = ''
+
+        mode = 'a' if os.path.isfile(filepath) else 'w'
+        if_sheet_exists = 'replace' if mode == 'a' else None
+        with pd.ExcelWriter(filepath, mode=mode, if_sheet_exists=if_sheet_exists, engine='openpyxl') as writer:
+            frame.to_excel(
+                writer,
+                sheet_name='ref-standard',
+            )
+
+        # flat-standard sheet
+        frame = pd.concat([
+            pd.DataFrame({
+                'index': ['Ср. знач.', 'Дов. инт.'],
+                'l_0, мкм': [length['flat-standard'].stats.value, length['flat-standard'].stats.interval],
+            }).set_index('index', drop=True),
+            pd.DataFrame({
+                'index': [''],
+                'l_0, мкм': [''],
+            }).set_index('index', drop=True),
+            pd.DataFrame({
+                'index': np.arange(1, len(length['flat-standard'].value)+1),
+                'l_0, мкм': length['flat-standard'].value,
+            }).set_index('index', drop=True),
+        ])
+        frame.index.name = ''
+
+        mode = 'a' if os.path.isfile(filepath) else 'w'
+        if_sheet_exists = 'replace' if mode == 'a' else None
+        with pd.ExcelWriter(filepath, mode=mode, if_sheet_exists=if_sheet_exists, engine='openpyxl') as writer:
+            frame.to_excel(
+                writer,
+                sheet_name='flat-standard',
+            )
 
         # config sheet
         frame = pd.DataFrame({
